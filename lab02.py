29772fde4936fa4a3ae14133c14e48e4f1352981
FuZezhi <47625241+FuZezhi@users.noreply.github.com>
2019-04-25 09:52:26 +0200

    Add files via upload

---
+++ lab02.py
@@ -0,0 +1,37 @@
+#while True:
+ # try:
+  #  n=int(input("Enter a number:"))
+   # print("Your number is:",n)
+    #break
+  #except ValueError:
+   # print("You should give me a number!")
+
+
+
+# while True:
+#  try:
+#      str=input("Give me the file name:")
+#      fin=open(str,'r')
+#      for r in fin:
+#        print(r,end='')
+#  except FileNotFoundError:
+#    print("The given file name not be found !")
+
+
+# def wordcounter(fin,str):
+#     counter=0
+#     for row in fin:
+#         words=row.split(' ')
+#         for w in words:
+#             if str in w:
+#              counter+=1
+#     return counter
+# fin=open("input.txt",'r')
+# print(wordcounter(fin,'Lannister'))
+
+
+# import sys
+# for e in sys.argv:
+#     print(e)
+
+
